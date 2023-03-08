65561de5df0ec17c36dddd5f50cde4a30229994d
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2023-03-08 20:35:44 +0200

    Lab3. Basic Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {ContactsComponent} from "./contacts/contacts.component";
+import {Page404Component} from "./page404/page404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "contacts", component: ContactsComponent},
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "**", component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
