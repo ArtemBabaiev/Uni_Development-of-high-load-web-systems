2888c47e208b25cb0c130bba7bea20eedf386694
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2023-03-08 21:27:44 +0200

    Lab3. Title change impl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {Injectable, NgModule} from '@angular/core';
+import {RouterModule, RouterStateSnapshot, Routes, TitleStrategy} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {ContactsComponent} from "./contacts/contacts.component";
 import {Page404Component} from "./page404/page404.component";
+import {Title} from "@angular/platform-browser";
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent},
-  {path: "about", component: AboutComponent},
-  {path: "contacts", component: ContactsComponent},
+  {path: "home", component: HomeComponent, title: "Home Page"},
+  {path: "about", component: AboutComponent, title: "About Page"},
+  {path: "contacts", component: ContactsComponent, title: "Contacts Page"},
   {path: "", redirectTo: "/home", pathMatch: "full"},
-  {path: "**", component: Page404Component}
+  {path: "**", component: Page404Component, title: "404 Page"}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
+@Injectable({providedIn: 'root'})
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`My Application | ${title}`);
+    }
+  }
+}
