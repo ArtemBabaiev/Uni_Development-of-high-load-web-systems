6f7d63d8cb70ba796255be74c9e5862324a0d334
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2023-03-14 21:19:05 +0200

    Lab4. Basic form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import {ContactsComponent} from "./contacts/contacts.component";
 import {Page404Component} from "./page404/page404.component";
 import {Title} from "@angular/platform-browser";
 import {UserdataComponent} from "./userdata/userdata.component";
+import {UserFormComponent} from "./user-form/user-form.component";
 
 const routes: Routes = [
   {path: "home", component: HomeComponent, title: "Home Page"},
   {path: "about", component: AboutComponent, title: "About Page"},
   {path: "contacts", component: ContactsComponent, title: "Contacts Page"},
   {path: "users/:id", component: UserdataComponent, title: "User Data Page"},
+  {path: "userform", component: UserFormComponent, title: "User Form Page"},
   {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "**", component: Page404Component, title: "404 Page"}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -30,7 +30,9 @@ import { UserFormComponent } from './user-form/user-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
